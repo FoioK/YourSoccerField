7fddb9d28d107d7074aeb6a8ca9315721c8918c8
Wojciech Kozioł <7wojok7@gmail.com>
2018-10-29 19:28:06 +0100

    Import FormModule, ReactiveFormsModule

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from './component/login/login.component';
 import {AuthService} from "./service/auth.service";
 import {Configuration} from "./service/configuration";
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [AuthService, Configuration],
   bootstrap: [AppComponent]
