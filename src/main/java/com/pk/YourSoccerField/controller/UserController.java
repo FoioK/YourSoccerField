5d38d6792eac182aa9ae664f5d8b7ca06d5beee8
Wojciech Kozioł <7wojok7@gmail.com>
2018-11-06 21:48:39 +0100

    UserController -> create user

---
+++ src/main/java/com/pk/YourSoccerField/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.pk.YourSoccerField.controller;
+
+import com.pk.YourSoccerField.service.UserService;
+import com.pk.YourSoccerField.service.dtoModel.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(
+            value = "/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(
+                userService.createUser(userDTO),
+                HttpStatus.CREATED
+        );
+    }
+
+}
