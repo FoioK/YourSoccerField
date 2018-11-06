1dad0b57feb2844409c2e8e7c0b16b871b8f0d27
Wojciech Kozioł <7wojok7@gmail.com>
2018-11-06 21:47:43 +0100

    Required repository methods to create user

--- src/main/java/com/pk/YourSoccerField/repository/PermissionRepository.kt
+++ src/main/java/com/pk/YourSoccerField/repository/PermissionRepository.kt
@@ -1,7 +1,9 @@
 package com.pk.YourSoccerField.repository
 
 import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PermissionRepository {
 
     companion object {
--- src/main/java/com/pk/YourSoccerField/repository/UserRepository.kt
+++ src/main/java/com/pk/YourSoccerField/repository/UserRepository.kt
@@ -2,9 +2,42 @@ package com.pk.YourSoccerField.repository
 
 import com.pk.YourSoccerField.model.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.math.BigInteger
 import java.util.*
 
+@Repository
 interface UserRepository : PermissionRepository, JpaRepository<UserEntity, Long> {
 
     fun findByEmail(email: String): Optional<UserEntity>
+
+    @Query(value = findNextUserCode, nativeQuery = true)
+    @Transactional
+    fun findNextUserCode(): Optional<BigInteger>
+
+    @Modifying
+    @Transactional
+    @Query(value = updateNextUserCode)
+    fun updateNextUserCode(@Param("code") code: Long): Int
+
+    @Modifying
+    @Transactional
+    @Query(value = insertUserRole, nativeQuery = true)
+    fun insertUserRole(
+            @Param("userId") userId: Long,
+            @Param("roleId") roleId: Long): Int
+
+    companion object {
+
+        const val findNextUserCode = "SELECT next_code FROM user_code WHERE id = 1"
+
+        const val updateNextUserCode = "UPDATE UserCode uc SET next_code = :code WHERE id = 1"
+
+        const val insertUserRole = "INSERT INTO user_role (user_id, role_id) " +
+                "VALUES (:userId, :roleId)"
+    }
 }
