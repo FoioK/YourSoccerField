338a352921143434274e59e560cdcc8095073202
Kamil Kopacz <k.dasch@interia.pl>
2018-12-02 18:27:18 +0100

    added completed layout for filter

--- src/ui/src/app/component/main-page/main-page.component.ts
+++ src/ui/src/app/component/main-page/main-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { trigger, state, style, animate, transition } from "@angular/animations";
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  selector: "app-main-page",
+  templateUrl: "./main-page.component.html",
+  styleUrls: ["./main-page.component.css"],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hide', style({
+        opacity: 0,
+      })),
+      transition('show=>hide', [
+        animate('0.5s')
+      ]),
+      transition('hide=>show', [
+        animate('0.5s')
+      ])
+    ])
+  ]
 })
 export class MainPageComponent implements OnInit {
+  filterShow: boolean = false;
+  maxWidth: number = 100;
+  minWidth: number = 0;
+  maxLength: number = 100;
+  minLength: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
