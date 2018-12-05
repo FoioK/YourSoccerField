e284ccd633b5617c227a5a7aad0ee7c3babcf7f9
Wojciech Kozioł <7wojok7@gmail.com>
2018-12-05 09:58:53 +0100

    Access token converter and user auth converter.

---
+++ api/src/main/java/com/pk/YourSoccerField/util/CustomUserAuthenticationConverter.java
@@ -0,0 +1,87 @@
+package com.pk.YourSoccerField.util;
+
+import com.pk.YourSoccerField.model.CustomUserDetail;
+import com.pk.YourSoccerField.model.UserEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CustomUserAuthenticationConverter implements UserAuthenticationConverter {
+
+    private static final String ID = "id";
+    private static final String CODE = "code";
+    private static final String EMAIL = "email";
+    private static final String IS_ACTIVE = "isActive";
+    private static final String CREATE_TIME = "createTime";
+    private Collection<? extends GrantedAuthority> defaultAuthorities;
+
+    public void setDefaultAuthorities(String[] defaultAuthorities) {
+        this.defaultAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList(StringUtils.arrayToCommaDelimitedString(defaultAuthorities));
+    }
+
+    @Override
+    public Map<String, ?> convertUserAuthentication(Authentication userAuthentication) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put(
+                USERNAME,
+                userAuthentication.getName()
+        );
+
+        if (userAuthentication.getAuthorities() != null && !userAuthentication.getAuthorities().isEmpty())
+            response.put(
+                    AUTHORITIES,
+                    AuthorityUtils.authorityListToSet(userAuthentication.getAuthorities())
+            );
+
+        return response;
+    }
+
+    @Override
+    public Authentication extractAuthentication(Map<String, ?> map) {
+        if (map.containsKey(USERNAME))
+            return new UsernamePasswordAuthenticationToken(
+                    this.getUserDetail(map), "N/A",
+                    getAuthorities(map));
+
+        return null;
+    }
+
+    private CustomUserDetail getUserDetail(Map<String, ?> map) {
+        return new CustomUserDetail(this.getUserEntity(map));
+    }
+
+    private UserEntity getUserEntity(Map<String, ?> map) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(Long.valueOf(map.get(ID).toString()));
+        userEntity.setCode(Long.valueOf(map.get(CODE).toString()));
+        userEntity.setEmail((String) map.get(EMAIL));
+        userEntity.setActive(Boolean.valueOf(map.get(IS_ACTIVE).toString()));
+        userEntity.setCreateTime(LocalDateTime.parse((String) map.get(CREATE_TIME)));
+        userEntity.setPassword("");
+        userEntity.setGrantedAuthorityList(new ArrayList<>());
+
+        return userEntity;
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Map<String, ?> map) {
+        if (!map.containsKey(AUTHORITIES))
+            return defaultAuthorities;
+        Object authorities = map.get(AUTHORITIES);
+        if (authorities instanceof String)
+            return AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities);
+        if (authorities instanceof Collection)
+            return AuthorityUtils.commaSeparatedStringToAuthorityList(
+                    StringUtils.collectionToCommaDelimitedString((Collection<?>) authorities));
+        throw new IllegalArgumentException("Authorities must be either a String or a Collection");
+    }
+}
