002e40ea56dea2e5f9d69afc74ddd5e43498d947
Wojciech Kozioł <7wojok7@gmail.com>
2019-01-12 20:37:38 +0100

    Refactor swagger config

--- api/src/main/java/com/pk/ysf/config/SwaggerConfig.java
+++ api/src/main/java/com/pk/ysf/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SwaggerConfig {
 
     @Value("${project.name}")
     private String name;
@@ -33,7 +33,7 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.pk.YourSoccerField"))
+                .apis(RequestHandlerSelectors.basePackage("com.pk.ysf"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(this.getApiInfo());
