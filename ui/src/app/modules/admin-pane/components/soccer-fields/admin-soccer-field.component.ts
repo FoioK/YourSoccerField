bee05efe1ce804e120e8be2ab9fc70bf2b7f21d7
Wojciech Kozioł <7wojok7@gmail.com>
2019-01-15 13:48:07 +0100

    Init soccer field component in admin pane

---
+++ ui/src/app/modules/admin-pane/components/soccer-fields/admin-soccer-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-soccer-field',
+  templateUrl: './admin-soccer-field.component.html',
+  styleUrls: ['./admin-soccer-field.component.css']
+})
+export class AdminSoccerFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
