ee6339c29a0c53bb229932757f593eed1c450f0a
Kamil Kopacz <k.dasch@interia.pl>
2019-02-13 12:50:12 +0100

    common styles for login and registration component have been added

--- ui/src/app/modules/login/login.component.ts
+++ ui/src/app/modules/login/login.component.ts
@@ -10,7 +10,7 @@ import {HttpErrorResponse} from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.styles.css', './login.component.css']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
