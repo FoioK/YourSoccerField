430ff9d98ae12bd0f14121c022c8ec2e8c8d49b4
Wojciech Kozioł <7wojok7@gmail.com>
2019-02-20 10:54:12 +0100

    Fix booking authorization

--- api/src/main/java/com/pk/ysf/controller/BookingController.kt
+++ api/src/main/java/com/pk/ysf/controller/BookingController.kt
@@ -7,27 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.net.URI
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("\${spring.data.rest.base-path}/bookings")
-class BookingController @Autowired constructor(
+open class BookingController @Autowired constructor(
         private var bookingService: BookingService
 ) {
 
-//    @PostMapping(
-//            consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE],
-//            produces = [MediaType.APPLICATION_JSON_UTF8_VALUE]
-//    )
-//    @PreAuthorize("hasAuthority(T(com.pk.ysf.util.Permissions).BOOKINGS_POST_CREATE)")
-//    fun create(@RequestBody bookingInput: BookingInput): ResponseEntity<BookingDetails> {
-//        var bookingDetails: BookingDetails = this.bookingService.create(bookingInput)
-//
-//        return ResponseEntity
-//                .created(URI.create("/bookings/${bookingDetails.id}"))
-//                .body(bookingDetails)
-//    }
+    @PostMapping(
+            consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE],
+            produces = [MediaType.APPLICATION_JSON_UTF8_VALUE]
+    )
+    @PreAuthorize("hasAuthority(T(com.pk.ysf.util.Permissions).BOOKINGS_POST_CREATE)")
+    fun create(@RequestBody bookingInput: BookingInput): ResponseEntity<BookingDetails> {
+        val bookingDetails: BookingDetails = this.bookingService.create(bookingInput)
+
+        return ResponseEntity
+                .created(URI.create("/bookings/${bookingDetails.id}"))
+                .body(bookingDetails)
+    }
 
 }
