0af0ad679c5185ae370f7d65d509a4634772d2fc
Wojciech Kozioł <7wojok7@gmail.com>
2019-03-05 19:09:48 +0100

    Custom validator aspect

--- api/src/main/kotlin/com/pk/ysf/api/aspect/CustomValidator.kt
+++ api/src/main/kotlin/com/pk/ysf/api/aspect/CustomValidator.kt
@@ -1,15 +1,14 @@
 package com.pk.ysf.api.aspect
 
+import com.pk.ysf.apimodels.exception.InvalidInputException
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.IllegalArgumentException
 import javax.validation.ConstraintViolation
 import javax.validation.Validator
+import kotlin.streams.toList
 
 @Aspect
 @Component
@@ -19,30 +18,27 @@ class CustomValidator @Autowired constructor(
 
     @Before(value = "@annotation(com.pk.ysf.api.annotation.ValidInput)")
     @Throws(Throwable::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun validate(joinPoint: JoinPoint) {
         val result: List<String> = validateProcess(joinPoint.args.toList())
-        if (result.isEmpty()) throw IllegalArgumentException(result.toString()) else return
+        if (result.isNotEmpty()) throw InvalidInputException(
+                result.joinToString { it }
+        ) else return
     }
 
     private fun validateProcess(params: List<Any>): List<String> {
-        val message: List<String> = emptyList()
-        repeat(params.size) {
-            val constraints = validator.validate(params[it])
-            if (!constraints.isEmpty()) {
-                message.plus(formatMessage(constraints))
-            }
-        }
-
-        return message
+        return params.stream()
+                .map { validator.validate(it) }
+                .filter { it.isNotEmpty() }
+                .map { formatMessage(it) }
+                .toList()
     }
 
-    private fun formatMessage(input: MutableSet<ConstraintViolation<Any>>): List<String> {
-        val response: List<String> = emptyList()
-        input.forEach {
-            response.plus("${it.propertyPath} ${it.message}")
-        }
+    private fun formatMessage(input: MutableSet<ConstraintViolation<Any>>): String =
+            input
+                    .map { "${it.propertyPath} ${it.message}" }
+                    .toTypedArray()
+                    .joinToString(
+                            separator = ";"
+                    ) { it }
 
-        return response
-    }
 }
