5ada29f9888f41c1dad46bb497f1018fee2a8646
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-03 11:23:09 +0200

    Refactor

--- api/src/main/kotlin/com/pk/ysf/api/security/ResourceServerConfiguration.kt
+++ api/src/main/kotlin/com/pk/ysf/api/security/ResourceServerConfiguration.kt
@@ -1,4 +1,4 @@
-package com.pk.ysf.api.configuration
+package com.pk.ysf.api.security
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
