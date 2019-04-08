2c1b21263f70144010ddf16d50f670a392d6550e
Wojciech Kozioł <7wojok7@gmail.com>
2019-04-08 14:17:06 +0200

    Fix tests

--- api/src/test/kotlin/com/pk/ysf/api/service/impl/BookingServiceImplTest.kt
+++ api/src/test/kotlin/com/pk/ysf/api/service/impl/BookingServiceImplTest.kt
@@ -1,12 +1,16 @@
 package com.pk.ysf.api.service.impl
 
-import com.pk.ysf.api.data.SOCCER_FIELD_ID
+import com.pk.ysf.api.data.any
+import com.pk.ysf.api.data.bookingDetailsMock
+import com.pk.ysf.api.data.bookingInputMock
 import com.pk.ysf.api.data.bookingMock
-import com.pk.ysf.api.data.soccerFieldMockOptional
 import com.pk.ysf.api.repository.BookingRepository
-import com.pk.ysf.api.repository.SoccerFieldRepository
 import com.pk.ysf.api.service.mapper.booking.BookingInputToBooking
 import com.pk.ysf.api.service.mapper.booking.BookingToBookingDetails
+import com.pk.ysf.api.validation.booking.BookingValidator
+import com.pk.ysf.apimodels.dto.BookingDetails
+import com.pk.ysf.apimodels.dto.BookingInput
+import com.pk.ysf.apimodels.entity.Booking
 import de.jodamob.kotlin.testrunner.KotlinTestRunner
 import org.junit.Before
 import org.junit.Test
@@ -15,6 +19,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import kotlin.test.assertEquals
 
 @RunWith(KotlinTestRunner::class)
 class BookingServiceImplTest {
@@ -23,13 +28,13 @@ class BookingServiceImplTest {
     lateinit var bookingService: BookingServiceImpl
 
     @Mock
-    lateinit var soccerFieldRepository: SoccerFieldRepository
+    lateinit var bookingValidator: BookingValidator
 
     @Mock
     lateinit var bookingRepository: BookingRepository
 
     @Mock
-    lateinit var  bookingInputToBooking: BookingInputToBooking
+    lateinit var bookingInputToBooking: BookingInputToBooking
 
     @Mock
     lateinit var bookingToBookingDetails: BookingToBookingDetails
@@ -38,15 +43,29 @@ class BookingServiceImplTest {
     fun init() {
         MockitoAnnotations.initMocks(this)
 
-        Mockito.`when`(soccerFieldRepository.findById(SOCCER_FIELD_ID))
-                .thenReturn(soccerFieldMockOptional())
+        Mockito.`when`(bookingValidator.validate(any(BookingInput::class.java)))
+                .thenAnswer { true }
+        Mockito.`when`(bookingInputToBooking.map(any(BookingInput::class.java)))
+                .thenReturn(bookingMock())
 
-        Mockito.`when`(bookingRepository.findAllByDate(soccerFieldId = SOCCER_FIELD_ID))
-                .thenReturn(listOf(bookingMock()))
+        Mockito.`when`(bookingRepository.save(any(Booking::class.java)))
+                .thenReturn(bookingMock())
+
+        Mockito.`when`(bookingToBookingDetails.map(any(Booking::class.java)))
+                .thenReturn(bookingDetailsMock())
     }
 
     @Test
     fun shouldCreateBooking() {
-//        val bookingDetails: BookingDetails = bookingService.create(correctBookingInput())
+        val expectedBookingDetails: BookingDetails = bookingDetailsMock()
+        val bookingDetails: BookingDetails = bookingService.create(bookingInputMock())
+
+        assertEquals(expectedBookingDetails.id, bookingDetails.id)
+        assertEquals(expectedBookingDetails.userCode, bookingDetails.userCode)
+        assertEquals(expectedBookingDetails.startDate, bookingDetails.startDate)
+        assertEquals(expectedBookingDetails.executionTime, bookingDetails.executionTime)
+        assertEquals(expectedBookingDetails.amount, bookingDetails.amount)
+        assertEquals(expectedBookingDetails.isPayed, bookingDetails.isPayed)
+        assertEquals(expectedBookingDetails.soccerField, bookingDetails.soccerField)
     }
 }
