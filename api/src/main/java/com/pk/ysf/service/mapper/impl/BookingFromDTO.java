d8b0a9072fc0b662c0aa421a97faf09066c4f6d7
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-14 15:55:58 +0200

    Add camel, oauth dependencies. Fix compile

--- api/src/main/java/com/pk/ysf/service/mapper/impl/BookingFromDTO.java
+++ api/src/main/java/com/pk/ysf/service/mapper/impl/BookingFromDTO.java
@@ -1,10 +1,10 @@
 package com.pk.ysf.service.mapper.impl;
 
+import com.pk.ysf.api.model.entity.Booking;
 import com.pk.ysf.api.repository.SoccerFieldRepository;
-import com.pk.ysf.api.service.mapper.BaseMapper;
 import com.pk.ysf.api.model.exception.MissingEntityException;
-import com.pk.ysf.apimodels.entity.Booking;
 import com.pk.ysf.api.model.entity.SoccerField;
+import com.pk.ysf.api.service.mapper.BaseMapper;
 import com.pk.ysf.service.dtoModel.BookingDTO;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
