ef3ecf6fd791a8c0d8dcd5de2b45a19670e7f686
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-21 15:47:22 +0200

    Fix surface mapping

--- api/src/main/kotlin/com/pk/ysf/api/controller/SurfacesController.kt
+++ api/src/main/kotlin/com/pk/ysf/api/controller/SurfacesController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("\${spring.data.rest.base-path}/bookings")
+@RequestMapping("\${spring.data.rest.base-path}/surfaces")
 class SurfacesController @Autowired constructor(
         private val surfaceService: SurfaceService
 ) {
