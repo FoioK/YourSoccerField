839c2c75ef48e73afcc24c53b44a5c01b5e1b7a3
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-27 14:57:46 +0200

    Time formatter

--- api/src/main/kotlin/com/pk/ysf/api/util/DatePaterrns.kt
+++ api/src/main/kotlin/com/pk/ysf/api/util/DatePaterrns.kt
@@ -1,4 +1,10 @@
 package com.pk.ysf.api.util
 
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 const val FULL_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss"
 const val SHORT_DATE_PATTERN = "yyyy-MM-dd HH:mm"
+
+val TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm")
+val timeToString: (LocalTime) -> String = { time -> time.format(TIME_FORMAT) }
