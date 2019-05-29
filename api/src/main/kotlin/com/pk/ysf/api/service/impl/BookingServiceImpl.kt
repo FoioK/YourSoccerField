a4ef60fd61ee0e3e6c875c17b21f2292d2b35920
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-30 00:10:41 +0200

    Authorization annotate to the interface

--- api/src/main/kotlin/com/pk/ysf/api/service/impl/BookingServiceImpl.kt
+++ api/src/main/kotlin/com/pk/ysf/api/service/impl/BookingServiceImpl.kt
@@ -1,17 +1,16 @@
 package com.pk.ysf.api.service.impl
 
 import com.pk.ysf.api.annotation.ValidInput
+import com.pk.ysf.api.model.dto.BookingDetails
+import com.pk.ysf.api.model.dto.BookingInput
+import com.pk.ysf.api.model.entity.Booking
+import com.pk.ysf.api.model.exception.ValidationException
 import com.pk.ysf.api.repository.BookingRepository
 import com.pk.ysf.api.service.mapper.booking.BookingInputToBooking
 import com.pk.ysf.api.service.mapper.booking.BookingToBookingDetails
 import com.pk.ysf.api.service.spec.BookingService
 import com.pk.ysf.api.validation.booking.BookingValidator
-import com.pk.ysf.api.model.dto.BookingDetails
-import com.pk.ysf.api.model.dto.BookingInput
-import com.pk.ysf.api.model.entity.Booking
-import com.pk.ysf.api.model.exception.ValidationException
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestBody
 
@@ -23,7 +22,6 @@ class BookingServiceImpl @Autowired constructor(
         private val bookingToBookingDetails: BookingToBookingDetails
 ) : BookingService {
 
-    @PreAuthorize("hasAuthority(T(com.pk.ysf.api.security.Permissions).BOOKINGS_POST_CREATE)")
     @ValidInput
     override fun create(@RequestBody bookingInput: BookingInput): BookingDetails {
         if (!bookingValidator.validate(bookingInput)) {
--- api/src/main/kotlin/com/pk/ysf/api/service/spec/BookingService.kt
+++ api/src/main/kotlin/com/pk/ysf/api/service/spec/BookingService.kt
@@ -2,9 +2,11 @@ package com.pk.ysf.api.service.spec
 
 import com.pk.ysf.api.model.dto.BookingDetails
 import com.pk.ysf.api.model.dto.BookingInput
+import org.springframework.security.access.prepost.PreAuthorize
 
 interface BookingService {
 
+    @PreAuthorize("hasAuthority(T(com.pk.ysf.api.security.Permissions).BOOKINGS_POST_CREATE)")
     fun create(bookingInput: BookingInput): BookingDetails
 
 }
