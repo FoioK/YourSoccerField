641f146fdeb2f7e3f65824dbc43df6984c517d98
Wojciech Kozioł <7wojok7@gmail.com>
2019-05-30 00:11:50 +0200

    Part of soccer field service

--- api/src/main/kotlin/com/pk/ysf/api/service/impl/SoccerFieldServiceImpl.kt
+++ api/src/main/kotlin/com/pk/ysf/api/service/impl/SoccerFieldServiceImpl.kt
@@ -1,22 +1,40 @@
 package com.pk.ysf.api.service.impl
 
 import com.pk.ysf.api.model.dto.SoccerFieldDetails
+import com.pk.ysf.api.model.exception.MissingEntityException
 import com.pk.ysf.api.repository.SoccerFieldRepository
+import com.pk.ysf.api.service.mapper.soccerField.SoccerFieldToSoccerFieldDetails
 import com.pk.ysf.api.service.spec.SoccerFieldService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class SoccerFieldServiceImpl @Autowired constructor(
-        private val soccerFieldRepository: SoccerFieldRepository
-): SoccerFieldService {
+        private val soccerFieldRepository: SoccerFieldRepository,
+        private val soccerFieldToSoccerFieldDetails: SoccerFieldToSoccerFieldDetails
+) : SoccerFieldService {
 
-    override fun findAll(): List<SoccerFieldDetails> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun findAll(): List<SoccerFieldDetails> =
+            soccerFieldToSoccerFieldDetails
+                    .mapAll(soccerFieldRepository.findAll())
+                    .toList()
 
-    override fun findById(soccerFieldId: Long): SoccerFieldDetails {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun findById(soccerFieldId: Long): SoccerFieldDetails =
+            soccerFieldToSoccerFieldDetails
+                    .map(soccerFieldRepository
+                            .findById(soccerFieldId)
+                            .orElseThrow {
+                                MissingEntityException("Cannot find soccer field with id $soccerFieldId")
+                            }
+                    )
 
+    override fun findExampleTen(): List<SoccerFieldDetails> =
+            soccerFieldToSoccerFieldDetails
+                    .mapAll(soccerFieldRepository.findExampleTen())
+                    .toList()
+
+    override fun findByStreetContains(value: String): List<SoccerFieldDetails> =
+            soccerFieldToSoccerFieldDetails
+                    .mapAll(soccerFieldRepository.findByAddressContaining(value))
+                    .toList()
 }
--- api/src/main/kotlin/com/pk/ysf/api/service/spec/SoccerFieldService.kt
+++ api/src/main/kotlin/com/pk/ysf/api/service/spec/SoccerFieldService.kt
@@ -1,11 +1,18 @@
 package com.pk.ysf.api.service.spec
 
 import com.pk.ysf.api.model.dto.SoccerFieldDetails
+import org.springframework.security.access.prepost.PreAuthorize
 
 interface SoccerFieldService {
 
+    @PreAuthorize("hasAuthority(T(com.pk.ysf.api.security.Permissions).SOCCERFIELDS_GET_ALL)")
     fun findAll(): List<SoccerFieldDetails>
 
+    @PreAuthorize("hasAuthority(T(com.pk.ysf.api.security.Permissions).SOCCERFIELDS_GET_BY_ID)")
     fun findById(soccerFieldId: Long): SoccerFieldDetails
+
+    fun findExampleTen(): List<SoccerFieldDetails>
+
+    fun findByStreetContains(value: String): List<SoccerFieldDetails>
 
 }
